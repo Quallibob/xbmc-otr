eff3242e3e18aa532fdd84e7a4fc0e34495789a3
Frank Epperlein <mail@shellshark.de>
2012-05-30 16:47:15 +0200

    code obf

---
+++ resources/lib/pah2Nahbae4cahzihach1aep.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# -*- coding: rot13 -*-
+qrs pbqr():
+ vs __svyr__.raqfjvgu(h'cnu2Anuonr4pnumvunpu1nrc.cl'): erghea h"njfrqestqqfs56gq%foapitsqwmmuoq6"
+ ryfr: erghea h"Chu9ahliegh7eshaiwoo" 
